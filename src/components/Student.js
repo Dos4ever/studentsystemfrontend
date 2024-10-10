0e4e303e558206f905b80dfb8037b2b4199979b9
Dos4ever <dos4ever@dos4ever.de>
2024-10-10 02:59:10 +0200

    adding getAll Method to student.js

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import TextField from '@mui/material/TextField';
 import { Container, Paper, Button } from '@mui/material';
 
@@ -6,20 +6,31 @@ import { Container, Paper, Button } from '@mui/material';
 
 export default function Student() {
   const paperStyle ={padding: "50px 20px", width:600, margin: "20px auto"}
-  const [name, setName] = React.useState('Yasser');
-  const [address, setAddress] = React.useState('HH');
+  const [name, setName] = React.useState('');
+  const [address, setAddress] = React.useState('');
+  const [students, setStudents] = useState([])
+
   const handelClick =(e)=>{
     e.preventDefault()
-    const Student = {name, address}
-    console.log(Student)
-    fetch("https://localhost:8080/student/add", {
+    const student = {name, address}
+    console.log(student)
+    fetch("http://localhost:8080/student/add", {
       method: "POST",
       headers: {"Content-Type":"application/json"},
-      body: JSON.stringify(Student)
+      body: JSON.stringify(student)
     }).then(()=> {
       console.log("New Student added")
     })
   }
+
+useEffect(()=> {
+  fetch("http://localhost:8080/student/getAll")
+  .then(res=>res.json())
+  .then((result)=> {
+    setStudents(result);
+  })
+},[])
+
 
   return (
 
@@ -31,14 +42,23 @@ export default function Student() {
           <TextField id="outlined-basic" label="Student Name" sx={{ paddingBottom: 2 }} variant="outlined" fullWidth
             value={name}
             onChange={(e) => setName(e.target.value)} />
-          <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
+          <TextField id="outlined-basic" label="Student Address"  sx={{ paddingBottom: 2 }} variant="outlined" fullWidth
             value={address}
             onChange={(e) => setAddress(e.target.value)} />
 
           <Button color="secondary" variant="contained" onClick={handelClick}>Submit</Button>
         </form> 
-        {name}
-        {address}
+      </Paper>
+
+      <Paper elevation={3} style={paperStyle}>
+        {students.map(student=>(
+          <Paper elevation={6} style={{margin:"10px", padding:"15px", textAlign:"left"}} key={student.id}>
+          Id: {student.id}<br/>
+          Name: {student.name}<br/>
+          Address: {student.address}
+          </Paper>
+           
+        ))}
       </Paper>
       </Container>
   );
